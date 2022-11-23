2857f09f75578c56059d5a62237c9526f7770a8c
Felipe Gavilan <elmejor_887@hotmail.com>
2022-11-22 20:43:34 -0400

    Add project files.

---
+++ MVCAndSweetAlert2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCAndSweetAlert2.Models;
+using System.Diagnostics;
+
+namespace MVCAndSweetAlert2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string Name)
+        {
+            return RedirectToAction("Completed", new { Name });
+        }
+
+        public IActionResult Completed(string Name)
+        {
+            return View("Completed", Name);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
